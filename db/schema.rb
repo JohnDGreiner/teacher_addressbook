7f36b5a53337e0d5301f523283bf310e0f8c12d3
JohnDGreiner <john.d.greiner@gmail.com>
2015-06-09 13:23:00 -0400

    #shipit, create teacher model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150609171619) do
+ActiveRecord::Schema.define(version: 20150609172133) do
 
   create_table "parents", force: :cascade do |t|
     t.string   "first_name"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20150609171619) do
     t.integer  "teacher_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "teachers", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
